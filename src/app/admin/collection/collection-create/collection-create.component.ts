d91a0ef2c1c81a92413693e0fa8c5d3b6f76b7b1
vasant2023 <vasant@coronation.in>
2023-08-17 13:21:05 +0530

    Changes of search and category id is completed

--- src/app/admin/collection/collection-create/collection-create.component.ts
+++ src/app/admin/collection/collection-create/collection-create.component.ts
@@ -59,8 +59,7 @@ export class CollectionCreateComponent implements OnInit {
     ],
   };
 
-  accordionList: { title: string, text: string }[] = [{ title: '', text: '' }];
-
+  accordionList: { title: string; text: string }[] = [{ title: "", text: "" }];
 
   collectionObj: any = {
     collection: "",
@@ -74,26 +73,26 @@ export class CollectionCreateComponent implements OnInit {
     description: "",
     short_description: "",
     item_IDs: [],
-    laminate_IDs:[],
-    hardware_IDs:[],
+    laminate_IDs: [],
+    hardware_IDs: [],
     warranty_detail: "",
     meta_description: "",
     meta_keywords: "",
     banner_image: (File = null),
     specification_image: (File = null),
     brochure: (File = null),
-    collection_images : [],
+    collection_images: [],
     sort_order: "",
     status: 1,
     warranty_time: "",
     tag_line: "",
     warranty_duration: "",
-    category_ID: [],
-    accordionList : [ ],
+    category_ID: "",
+    accordionList: [],
     accordian: {
       title: "",
       text: "",
-    }
+    },
   };
   collection_ID = "";
 
@@ -105,15 +104,24 @@ export class CollectionCreateComponent implements OnInit {
     private toastr: ToastrService,
     private http: HttpClient,
     private dragulaService: DragulaService
-  ) {}
+  ) { }
 
   collectionForm: FormGroup;
 
   isLoading = false;
   itemList: any = [];
-  laminatesList:any =[];
+  laminatesList: any = [];
   categoryList: any = [];
-  hardwaresList:any = [];
+  hardwaresList: any = [];
+
+  // Testing
+
+  filteredItemList: any[] = []; 
+  filteredLaminateList: any[] = []; 
+  filteredHardwareList: any[] = []; 
+  searchItem: string;
+  searchLaminates:string;
+  searchHardwares:string;
 
   ngOnInit() {
     this.getCollectionID();
@@ -122,6 +130,32 @@ export class CollectionCreateComponent implements OnInit {
     this.getCategories();
     this.getAllLaminates();
     this.getAllHardwares();
+  }
+
+  filterItems() {
+    if (this.searchItem) {
+      this.filteredItemList = this.itemList.filter(item =>
+        item.item.toLowerCase().includes(this.searchItem.toLowerCase())
+      );
+    } else {
+      this.filteredItemList = this.itemList;
+    }
+  }
+
+  filterLaminates(){
+    if(this.searchLaminates) {
+      this.filteredLaminateList = this.laminatesList.filter(laminate => laminate.laminate.toLowerCase().includes(this.searchLaminates.toLocaleLowerCase()))
+    } else {
+      this.filteredLaminateList = this.laminatesList
+    }
+  }
+
+  filterHardwares(){
+    if(this.searchHardwares) {
+      this.filteredHardwareList = this.hardwaresList.filter(hardware => hardware.hardware.toLowerCase().includes(this.searchHardwares.toLocaleLowerCase()))
+    } else {
+      this.filteredHardwareList = this.hardwaresList
+    }
   }
 
   public onChange({ editor }: ChangeEvent) {
@@ -137,10 +171,10 @@ export class CollectionCreateComponent implements OnInit {
   }
 
   item_images(event) {
-    this.collectionObj.collection_images = []; 
+    this.collectionObj.collection_images = [];
     var files = event.srcElement.files;
     for (var key in files) {
-      if (typeof (files[key].type) != "undefined") {
+      if (typeof files[key].type != "undefined") {
         this.collectionObj.collection_images.push(files[key]);
       }
     }
@@ -158,11 +192,12 @@ export class CollectionCreateComponent implements OnInit {
 
   getCategories() {
     this.adminService
-      .nestedCategoryList()
+      .getCollectioncategories()
       .subscribe(
         (response: { success: number; message: string; categories: [] }) => {
           if (response.success == 1) {
             this.categoryList = response.categories;
+            console.log(this.categoryList);
           }
         }
       );
@@ -175,26 +210,36 @@ export class CollectionCreateComponent implements OnInit {
         (response: { success: number; message: string; items: [] }) => {
           if (response.success == 1) {
             this.itemList = response.items;
+            this.filteredItemList = this.itemList
           }
         }
       );
   }
 
-  getAllLaminates(){
-    this.adminService.getLaminates().subscribe((response:{success:number, message:string, laminates:[]}) => {
-      if(response.success == 1) {
-        this.laminatesList = response.laminates;
-      }
-    })
+  getAllLaminates() {
+    this.adminService
+      .getLaminates()
+      .subscribe(
+        (response: { success: number; message: string; laminates: [] }) => {
+          if (response.success == 1) {
+            this.laminatesList = response.laminates;
+            this.filteredLaminateList = this.laminatesList
+          }
+        }
+      );
   }
 
-  getAllHardwares(){
-    this.adminService.getHardwares().subscribe((response:{success:number, message:string, hardwares:[]}) => {
-      if(response.success == 1){
-        this.hardwaresList = response.hardwares;
-        console.log(this.hardwaresList)
-      }
-    })
+  getAllHardwares() {
+    this.adminService
+      .getHardwares()
+      .subscribe(
+        (response: { success: number; message: string; hardwares: [] }) => {
+          if (response.success == 1) {
+            this.hardwaresList = response.hardwares;
+            this.filteredHardwareList = this.hardwaresList
+          }
+        }
+      );
   }
 
   collectionDetails() {
@@ -219,19 +264,19 @@ export class CollectionCreateComponent implements OnInit {
 
             this.collectionObj.hardwares.forEach((value) => {
               this.collectionObj.hardware_IDs.push(parseInt(value.hardware_ID));
-            })
+            });
           }
         }
       );
   }
 
   addAccordion() {
-    const newAccordion = { title: '', text: '' };
+    const newAccordion = { title: "", text: "" };
     this.accordionList.push(newAccordion);
     this.collectionObj.accordionList = this.accordionList;
-    console.log(this.accordionList)
+    console.log(this.accordionList);
   }
-  
+
   deleteAccordion(index: number) {
     this.accordionList.splice(index, 1);
   }
@@ -240,10 +285,10 @@ export class CollectionCreateComponent implements OnInit {
     console.log(this.collectionObj);
     // return false
     this.collectionObj.accordionList = this.accordionList;
-    if(form.valid){
+    if (form.valid) {
       if (this.isLoading == false) {
         this.isLoading = true;
-  
+
         let formData = new FormData();
         formData.append("apiId", environment.apiId);
         formData.append("from_app", "true");
@@ -260,12 +305,12 @@ export class CollectionCreateComponent implements OnInit {
           "short_description",
           this.collectionObj.short_description
         );
-  
+
         var sort_order = 1;
         var items = [];
         var laminates = [];
         var hardwares = [];
-  
+
         this.collectionObj.item_IDs.forEach((single_ID) => {
           var Obj = {
             item_ID: single_ID,
@@ -277,38 +322,47 @@ export class CollectionCreateComponent implements OnInit {
 
         this.collectionObj.laminate_IDs.forEach((single_ID) => {
           var Obj = {
-            laminate_ID : single_ID,
+            laminate_ID: single_ID,
             sort_order: sort_order,
-          }
+          };
           laminates.push(Obj);
           sort_order++;
-        })
+        });
 
         this.collectionObj.hardware_IDs.forEach((single_ID) => {
           var Obj = {
-            hardware_ID : single_ID,
+            hardware_ID: single_ID,
             sort_order: sort_order,
-          }
+          };
           hardwares.push(Obj);
           sort_order++;
-        })
-  
+        });
+
         formData.append("items", JSON.stringify(items));
         formData.append("laminates", JSON.stringify(laminates));
         formData.append("hardwares", JSON.stringify(hardwares));
         formData.append("warranty_detail", this.collectionObj.warranty_detail);
-        formData.append("meta_description", this.collectionObj.meta_description);
+        formData.append(
+          "meta_description",
+          this.collectionObj.meta_description
+        );
         formData.append("meta_keywords", this.collectionObj.meta_keywords);
         formData.append("banner_image", this.collectionObj.banner_image);
-        formData.append("specification_image", this.collectionObj.specification_image);
+        formData.append(
+          "specification_image",
+          this.collectionObj.specification_image
+        );
         formData.append(
           "banner_image_mobile",
           this.collectionObj.banner_image_mobile
         );
         formData.append("brochure", this.collectionObj.brochure);
-  
+
         for (var index in this.collectionObj.collection_images) {
-          formData.append("collection_images[" + index + "]", this.collectionObj.collection_images[index]);
+          formData.append(
+            "collection_images[" + index + "]",
+            this.collectionObj.collection_images[index]
+          );
         }
         formData.append("image_alttext", this.collectionObj.image_alttext);
         formData.append("sort_order", this.collectionObj.sort_order);
@@ -319,10 +373,12 @@ export class CollectionCreateComponent implements OnInit {
           "warranty_duration",
           this.collectionObj.warranty_duration
         );
-        formData.append("categories", this.collectionObj.category_ID);
-        formData.append("accordionList", JSON.stringify(this.collectionObj.accordionList));
-        
-  
+        formData.append("category_ID", this.collectionObj.category_ID);
+        formData.append(
+          "accordionList",
+          JSON.stringify(this.collectionObj.accordionList)
+        );
+
         if (this.collection_ID) {
           formData.append("collection_ID", this.collection_ID);
           this.adminService
@@ -332,7 +388,6 @@ export class CollectionCreateComponent implements OnInit {
                 this.router.navigate(["admin/collections"]);
               }
               this.isLoading = false;
-  
             });
         } else {
           this.adminService
@@ -342,11 +397,9 @@ export class CollectionCreateComponent implements OnInit {
                 this.router.navigate(["admin/collections"]);
               }
               this.isLoading = false;
-  
             });
         }
       }
     }
   }
-
 }
